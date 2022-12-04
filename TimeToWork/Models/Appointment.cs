95df145de043cbe2da8825ad9cb7f2e84f89d603
Денис Зайдун <70152795+DenisZaydun@users.noreply.github.com>
2022-12-04 16:15:48 +0200

    add models

---
+++ TimeToWork/Models/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeToWork.Models
+{
+	public class Appointment
+	{
+		public int AppointmentId { get; set; }
+		public int ServiceId { get; set; }
+		public int ClientId { get; set; }
+
+		[Display(Name = "Дата")]
+		public DateTime Date { get; set; }
+
+		public Service? Service { get; set; }
+		public Client? Client { get; set; }
+	}
+}
---
+++ TimeToWork/Models/Service.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeToWork.Models
+{
+	public class Service
+	{
+		public int ServiceId { get; set; }
+
+		[Display(Name = "Назва послуги")]
+		[StringLength(50, MinimumLength = 3)]
+		public string? ServiceName { get; set; }
+
+		[Display(Name = "Короткий опис")]
+		public string? ShortDescription { get; set; }
+
+		[Display(Name = "Опис")]
+		public string? Description { get; set; }
+		public ICollection<Appointment>? Appointments { get; set; }
+		public ICollection<ServiceAssignment>? ServiceAssignments { get; set; }
+	}
+}
